47e5ac8ce1d56ac528970e51d2ec1d3e29378557
HakkerzX <samohinegor@gmail.com>
2020-06-04 22:23:00 +0300

    Добавьте файлы проекта.

---
+++ LB_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>();
+
+            for (int i = 1; i < 64; i++)
+            {
+                array.Add(i);
+            }
+
+            array.AsParallel().ForAll(x =>
+            {
+                double value = Math.Pow(x, 2);
+                Console.WriteLine($"{x}^2 = {value}");
+            });
+            Console.Read();
+        }
+    }
+}
