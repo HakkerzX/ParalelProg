88917338cbda27fcc7cc340b499c33cf1557ef31
HakkerzX <samohinegor@gmail.com>
2020-06-04 22:06:52 +0300

    Добавьте файлы проекта.

---
+++ LB_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LB_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TaskLab().Lab3();
+        }
+        public class TaskLab
+        {
+            public void Lab3()
+            {
+                var tokenSource = new CancellationTokenSource();
+                var token = tokenSource.Token;
+                new Task(() =>
+                {
+                    int i = 0;
+                    while (!token.IsCancellationRequested)
+                    {
+                        i++;
+                        Console.WriteLine($"Ожидайте {i} шаг!!!");
+                        Thread.Sleep(100);
+                    }
+                }).Start();
+                Thread.Sleep(1000);
+                tokenSource.Cancel();
+                Console.WriteLine("Операции завершены");
+                Console.Read();
+            }
+        }
+    }
+}
